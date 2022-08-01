948ffd4a33e9244959bea5cff6e0ef057208135f
Sebastian <sebastianlindbergl@gmail.com>
2022-08-01 19:30:30 +0200

    Prep for heroku

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT, () => {
+  console.log("Listening on port:", PORT);
+});
